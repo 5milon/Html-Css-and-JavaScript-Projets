d7c3b1bc573a47f024d0630102109215e411cf21
5milon <5milon5ali@gmail.com>
2025-01-16 14:35:15 +0600

    wishlist compute

--- Project-01/js/main.js
+++ Project-01/js/main.js
@@ -6,6 +6,25 @@ window.onload = () => {
   };
   accountMenu.onmouseout = () => {
     const dropdown = document.getElementById("account-menu");
-    dropdown.style.display ='none'
+    dropdown.style.display = "none";
+  };
+
+  /* Open Cart*/
+
+  const cartBtn = document.getElementById("cart");
+  cartBtn.onclick = () => {
+    const overlay = document.getElementById("cart-overlay");
+    const cartList = document.getElementById("cart-list");
+    overlay.style.display = "block";
+    cartList.style.animation = "cartList 0.4s linear forwards";
+  };
+
+  const closeCart = document.getElementById("close-cart");
+
+  closeCart.onclick = () => {
+   const cartList = document.getElementById("cart-list");
+   cartList.style.animation = "cartlistcolose 0.4s linear forwards";
+   const overlay = document.getElementById("cart-overlay");
+   overlay.classList = "animate__animated animate__fadeOut";
   };
 };

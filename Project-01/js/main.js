ab5b1b47004f34add4b7e7666957311085461db9
5milon <5milon5ali@gmail.com>
2025-01-22 22:52:17 +0600

    add checkout file almost finish

--- Project-01/js/main.js
+++ Project-01/js/main.js
@@ -22,10 +22,37 @@ window.onload = () => {
   const closeCart = document.getElementById("close-cart");
 
   closeCart.onclick = () => {
-   const cartList = document.getElementById("cart-list");
-   cartList.style.animation = "cartlistcolose 0.4s linear forwards";
-   const overlay = document.getElementById("cart-overlay");
-  //  overlay.classList = "animate__animated animate__fadeOut";
-   overlay.style.display = "none";
+    const cartList = document.getElementById("cart-list");
+    cartList.style.animation = "cartlistcolose 0.4s linear forwards";
+    const overlay = document.getElementById("cart-overlay");
+    //  overlay.classList = "animate__animated animate__fadeOut";
+
+    setTimeout(() => {
+      overlay.style.display = "none";
+    }, 400);
   };
 };
+
+
+// Increase and Decrease Quantity
+let increase = document.getElementsByClassName("increase-qnt"); // Increase Button
+let decrease = document.getElementsByClassName("decrease-qnt"); // Decrease Button
+let quantity = document.getElementsByClassName("qnt"); // Quantity Input
+
+for(let btn of increase){
+  btn.onclick = () => {
+    let div = btn.parentElement;
+    let lable = div.getElementsByClassName("qnt")[0];
+    let text = Number(lable.innerHTML);
+    lable.innerHTML = text + 1;
+  }
+}
+
+for(let btn of decrease){
+  btn.onclick = () => {
+    let div = btn.parentElement;
+    let lable = div.getElementsByClassName("qnt")[0];
+    let text = Number(lable.innerHTML);
+    lable.innerHTML = text - 1;
+  }
+}
